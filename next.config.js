075110a28cc135e740d18bfc0483b914e55e290e
Angelo Reale <varsnothing@gmail.com>
2024-01-17 20:12:56 +0100

    vn(feat) add gh oauth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["next-auth"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/api/auth/signin",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
