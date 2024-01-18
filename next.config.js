f11cd2ddf1295bd7aee65d1a12fd202161ce4c42
Angelo Reale <varsnothing@gmail.com>
2024-01-18 12:33:17 +0100

    vn(feat) new signin home

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/api/auth/signin",
+        destination: "/signin",
         permanent: true,
       },
     ];
