6c956ad257bb16e06e3bed38e0e63c6d4a2cb178
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-01-27 00:30:11 +0100

    ar(feat) provider

--- lib/auth/constants.ts
+++ lib/auth/constants.ts
@@ -2,6 +2,7 @@
 import type { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
+import InstagramProvider from "next-auth/providers/instagram";
 
 export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
@@ -10,11 +11,11 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    // EmailProvider({
-    //   server: process.env.EMAIL_SERVER as string,
-    //   from: process.env.EMAIL_FROM as string,
-    //   // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-    // }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER as string,
+      from: process.env.EMAIL_FROM as string,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
     InstagramProvider({
     clientId: process.env.INSTAGRAM_CLIENT_ID,
     clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
