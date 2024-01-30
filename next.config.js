2d4e8b518db009b36383502e0027b126ef19fd51
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-01-30 05:53:46 +0100

    ar(audio)

--- next.config.js
+++ next.config.js
@@ -58,75 +58,75 @@ const config = {
             {"source": "/app/:match*", "destination": "https://alpha.dreampip.com/:match*"},
             {
                 source: '/api/nexus/audio/0',
-                destination: 'http://66.135.5.207:8002/main',
+                destination: 'https://radio.media.infra.remometro.com/0',
             },
             {
                 source: '/api/nexus/audio/1',
-                destination: 'https://radio.media.infra.dreampip.com/0',
+                destination: 'https://radio.media.infra.remometro.com/0',
             },
             {
                 source: '/api/nexus/audio/1',
-                destination: 'https://radio.media.infra.dreampip.com/1',
+                destination: 'https://radio.media.infra.remometro.com/1',
             },
             {
                 source: '/api/nexus/audio/2',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/2',
             },
             {
                 source: '/api/nexus/audio/4',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/3',
             },
             {
                 source: '/api/nexus/audio/5',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/4',
             },
             {
                 source: '/api/nexus/audio/6',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/5',
             },
             {
                 source: '/api/nexus/audio/7',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/5',
             },
             {
                 source: '/api/nexus/audio/8',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/6',
             },
             {
                 source: '/api/nexus/audio/9',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/7',
             },
             {
                 source: '/api/nexus/audio/10',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/8',
             },
             {
                 source: '/api/nexus/audio/11',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/9',
             },
             {
                 source: '/api/nexus/audio/12',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/10',
             },
             {
                 source: '/api/nexus/audio/13',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/11',
             },
             {
                 source: '/api/nexus/audio/14',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/12',
             },
             {
                 source: '/api/nexus/audio/15',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/13',
             },
             {
                 source: '/api/nexus/audio/16',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/14',
             },
             {
                 source: '/api/nexus/audio/17',
-                destination: 'https://radio.media.infra.dreampip.com/2',
+                destination: 'https://radio.media.infra.remometro.com/15',
             },
             {
                 source: '/api/nexus/audio/3',
