0722a57ce186f8f42a8eae1b0ba51d30a9488005
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-02-08 11:05:03 +0100

    ar(qa) code review

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,14 @@ import { clsx } from 'clsx';
 import { RootProviders } from '@state';
 import { Comfortaa } from 'next/font/google';
 import './globals.css';
+import styles from '@styles/page.module.css';
 
-const comfortaa = Comfortaa({ subsets: ['latin'] });
+const comfortaa = Comfortaa({ subsets: ['latin'], variable: '--nexus-font' });
 
 const classes = clsx({
   [comfortaa.className]: true,
+  [comfortaa.variable]: true,
+  [styles.text]: true,
 });
 
 export const metadata: Metadata = {
@@ -18,6 +21,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
       <body className={classes}>
         <RootProviders>{children}</RootProviders>
       </body>
