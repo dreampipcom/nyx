69d44068a1b15731f4a24c1e7d786f177b364204
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-07-12 00:57:25 +0100

    ar(feat) 2024 2nd semester rebase.

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -11,28 +11,28 @@ test('loads signin', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Sign in' }).click();
 
-  await expect(page.getByRole('button', { name: 'Sign in with Github' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Continue with GitHub' })).toBeVisible();
 });
 
-test('does signin with current nyx branch and hypnos dev', async ({ page }) => {
-  // hypnos auth mock
-  page.route('https://dp-nyx.vercel.app/api/auth/signin/github**', async (route, request) => {
-    const callbackUrl = new URL('https://dp-nyx.vercel.app/?signedIn=true');
+// test('does signin with current nyx branch and hypnos dev', async ({ page }) => {
+//   // hypnos auth mock
+//   page.route('https://dp-nyx.vercel.app/api/auth/signin/github**', async (route, request) => {
+//     const callbackUrl = new URL('https://dp-nyx.vercel.app/?signedIn=true');
 
-    return route.fulfill({
-      status: 308,
-      headers: {
-        Location: callbackUrl,
-        'x-TEST': 'true',
-      },
-    });
-  });
+//     return route.fulfill({
+//       status: 308,
+//       headers: {
+//         Location: callbackUrl,
+//         'x-TEST': 'true',
+//       },
+//     });
+//   });
 
-  await page.goto('http://localhost:3000/');
+//   await page.goto('http://localhost:3000/');
 
-  await page.getByRole('button', { name: 'Sign in' }).click();
+//   await page.getByRole('button', { name: 'Sign in' }).click();
 
-  await page.getByRole('button', { name: 'Sign in with Github' }).click();
+//   await page.getByRole('button', { name: 'Continue with Github' }).click();
 
-  await expect(page.url()).toContain('signedIn=true');
-});
+//   await expect(page.url()).toContain('signedIn=true');
+// });
