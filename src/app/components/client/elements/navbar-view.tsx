66739d45237ec8716f0e2963f8f84aed7b9fa437
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-07-14 00:30:11 +0100

    ar(temp) auth stuff messy

--- src/app/components/client/elements/navbar-view.tsx
+++ src/app/components/client/elements/navbar-view.tsx
@@ -12,7 +12,6 @@ interface VNavBar {
 
 export const VNavBar = () => {
   const authContext = useContext(AuthContext);
-  // const { data: session } = useSession();
   const initd = useRef(false);
 
   const { authd, name } = authContext;
@@ -24,13 +23,7 @@ export const VNavBar = () => {
   }, []);
 
   const handleSignOut = async () => {
-    // unloadUser();
-    // await doSignOut();
   };
-
-  // if (!providers) return;
-
-  // if (typeof session === "undefined") return <span>Loading...</span>;
 
   if (authd)
     return (
@@ -44,6 +37,4 @@ export const VNavBar = () => {
         </div>
       </div>
     );
-
-  // return <button>Nav items !auth.d</button>;
 };
