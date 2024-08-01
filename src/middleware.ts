c629acf55429c6ec4dc50473b4696ca0602f8a3f
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-01 13:47:47 +0100

    ar(fix) apple stuff

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,12 +7,23 @@ export const config = {
 };
 
 export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const pkce = request.cookies.get('next-auth.pkce.code_verifier')
+
+  if (pkce?.value) {
+    response.cookies.set("next-auth.pkce.code_verifier", pkce.value, {
+      httpOnly: true,
+      sameSite: 'none',
+      path: '/',
+      secure: true,
+    });
+    // console.log({ pkce, response, to: request.nextUrl.pathname });
+  }
+
   return NextResponse.rewrite(
     new URL(
       `${process.env.REMOTE_DEV ? process.env.API_HOST_DEV : process.env.API_HOST}${request.nextUrl.pathname}${request.nextUrl.search}`,
     ),
-    {
-      request,
-    },
+    response
   );
 }
