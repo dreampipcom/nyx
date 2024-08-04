c45ac1cf5432cdc180b3181dd95a457565ecd3ef
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-04 14:27:50 +0100

    ar(feat) DPCP-27 DPCP-28: Icon Button Size, Custom Protocol

--- src/app/components/client/elements/link-decorator.tsx
+++ src/app/components/client/elements/link-decorator.tsx
@@ -4,6 +4,6 @@ import { Link as DPLink } from "@dreampipcom/oneiros"
 
 export const DPLinkDec = (props: any) => {
     return (
-      <DPLink host={process.env.NEXT_PUBLIC_NEXUS_HOST.replace('http://', '')} {...props} />
+      <DPLink host={process?.env?.NEXT_PUBLIC_NEXUS_HOST?.replace('http://', '')} {...props} />
     );
 };
