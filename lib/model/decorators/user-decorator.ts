ce75843f03b4fc240699b3c22ca71a968ae6f827
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-05 19:38:13 +0100

    ar(fix) bug https

--- lib/model/decorators/user-decorator.ts
+++
@@ -1,8 +0,0 @@
-p// user-decorator.ts input: user/s; output: decorated user/s;
-'use server';
-// import type { OrgSchema, UserSchema } from "@types";
-// import { getUserMeta } from "@controller";
-
-/* private */
-
-/* public */
