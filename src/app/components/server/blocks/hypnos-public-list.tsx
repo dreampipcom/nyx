b55fce398cd0b317cae687fadd73617c772874b2
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-05 17:28:18 +0100

    ar(feat) [DPCP-38] [DPCP-39] [DPCP-46]: Abstract Views

--- src/app/components/server/blocks/hypnos-public-list.tsx
+++ src/app/components/server/blocks/hypnos-public-list.tsx
@@ -1,16 +1,18 @@
 // hypnos-public-list.tsx
 'use server';
 import type { ICard } from '@dreampipcom/oneiros';
-import { VHPNPList } from '@elements/client';
+import { useContext } from 'react';
+import { ListView } from '@elements/client';
 import { loadHypnosPublicListings, addToFavorites } from '@gateway';
-import { HypnosPublicProvider } from '@state';
+import { HypnosPublicProvider, HypnosPublicContext } from '@state';
+import { ALoadPublicListings, AUnloadPublicListings, ADecoratePublicListings, AAddToFavoritePublicListings } from '@actions';
 
 export const CHPNPList = async () => {
   const listings: ICard[] = await loadHypnosPublicListings();
 
   return (
     <HypnosPublicProvider>
-      <VHPNPList listings={listings} />
+      <ListView listings={listings} fetchListings={loadHypnosPublicListings} loadListings={ALoadPublicListings} decListings={ADecoratePublicListings} unloadListings={AUnloadPublicListings} listingContext={HypnosPublicContext} addToFavorites={AAddToFavoritePublicListings} />
     </HypnosPublicProvider>
   );
 };
--- src/app/components/server/blocks/rm-list.tsx
+++ src/app/components/server/blocks/rm-list.tsx
@@ -1,16 +1,17 @@
 // list-controller.tsx
 'use server';
 import type { ICard } from '@dreampipcom/oneiros';
-import { VRMList } from '@elements/client';
-import { loadChars } from '@gateway';
-import { RickMortyProvider } from '@state';
+import { ListView } from '@elements/client';
+import { loadChars, addToFavorites } from '@gateway';
+import { RickMortyProvider, RMContext } from '@state';
+import { ALoadChars, AUnloadChars, ADecorateChars, AAddToFavoriteChars} from '@actions';
 
 export const CRMList = async () => {
   const characters: ICard[] = await loadChars();
 
   return (
     <RickMortyProvider>
-      <VRMList characters={characters} />
+      <ListView listings={characters} fetchListings={loadChars} loadListings={ALoadChars} decListings={ADecorateChars} unloadListings={AUnloadChars} listingContext={RMContext} addToFavorites={AAddToFavoriteChars} />
     </RickMortyProvider>
   );
 };
