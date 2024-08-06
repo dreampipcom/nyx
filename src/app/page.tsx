034cacda3691f5be2eebc64ba3edadc26b721835
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-06 14:00:21 +0100

    ar(fix) [DPCP-47]: DeepLink no default home service view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 // page.tsx
-import { DPPublicListings } from '@blocks/server';
 export default function Home() {
   return (
     <main>
       <section>
-        <DPPublicListings />
       </section>
     </main>
   );
