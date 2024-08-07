2ae59fea796193ac7681a14f6efd6b4011655c40
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-07 11:40:02 +0100

    ar(fix) [DPCP-71]: CSRF Fix

--- src/app/gateway/client/actions.ts
+++ src/app/gateway/client/actions.ts
@@ -1,13 +1,13 @@
 // actions.ts
 'use server';
 import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 
 export async function navigate(url: string) {
   redirect(url);
 }
- 
+
 export async function setCookie({ name, value }) {
   // Set cookie
-  cookies().set(name, value)
+  cookies().set(name, value);
 }
