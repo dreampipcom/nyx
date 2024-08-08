c84dcb750bc2884433267090f74a962bca07c8f8
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-08 15:21:28 +0100

    ar(fix) [DPTM-2]: Ensure autonomous Dev environment

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,9 @@ export const config = {
 };
 
 const allowedOrigins = {
-  [process.env.MAIN_URL]: process.env.MAIN_URL,
-  [process.env.NEXUS_HOST]: process.env.NEXUS_HOST,
-  [process.env.API_HOST]: process.env.API_HOST,
+  [`${process.env.MAIN_URL}`]: process.env.MAIN_URL,
+  [`${process.env.NEXUS_HOST}`]: process.env.NEXUS_HOST,
+  [`${process.env.API_HOST}`]: process.env.API_HOST,
 }
 
 
