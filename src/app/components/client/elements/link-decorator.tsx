a83f32312ae33876de605bb551f9ff3dc2681423
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-08 14:14:53 +0100

    ar(fix) [DPTM-2]: Ensure autonomous Dev environment

--- src/app/components/client/elements/link-decorator.tsx
+++ src/app/components/client/elements/link-decorator.tsx
@@ -4,6 +4,6 @@ import { Link as DPLink } from "@dreampipcom/oneiros"
 
 export const DPLinkDec = (props: any) => {
     return (
-      <DPLink host={process?.env?.NEXT_PUBLIC_NEXUS_HOST?.replace('https://', '').replace('http://', '')} {...props} />
+      <DPLink host={process?.env?.NEXT_PUBLIC_MAIN_URL?.replace('https://', '').replace('http://', '')} {...props} />
     );
 };
