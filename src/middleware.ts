c7bfa5767d0b2aa009d7b230da72e3d548a903af
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-21 21:18:04 +0100

    ar(feat) [DPCP-37]: Localization

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,9 +13,9 @@ export const config = {
 const blocklist = ['_next'];
 
 export default async function middleware(request: NextRequest) {
-  const url = request?.nextUrl?.pathname
+  const url = request?.nextUrl?.pathname;
   if (blocklist.some((pattern) => url?.includes(pattern))) {
-    console.log("--- PATTERN BOCKED: ---", url)
+    console.log('--- PATTERN BOCKED: ---', url);
     return NextResponse.next();
   }
   // if a response is returned, return it otherwise call `next()`
