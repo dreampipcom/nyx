c5aaf8ae87f2f5bbdd7fb59b86effb567920eb06
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-21 12:29:34 +0100

    ar(feat) [DPCP-37]: Localization

--- src/middleware.ts
+++ src/middleware.ts
@@ -86,7 +86,7 @@ export function middleware(request: NextRequest) {
 
     if (!headers) return NextResponse.rewrite(newUrl);
     const savedLocale = request?.cookies?.get('NEXT_LOCALE');
-    const newlocale = savedLocale?.value || acceptLanguage?.get(headers)?.toLocaleLowerCase() || 'en' as keyof localeMap;
+    const newlocale = savedLocale?.value || acceptLanguage?.get(headers)?.toLocaleLowerCase() || 'en' as keyof typeof localeMap;
     newUrl.pathname = newUrl?.pathname?.replace('/default', localeMap[newlocale] || newlocale);
 
     return NextResponse.redirect(newUrl);
