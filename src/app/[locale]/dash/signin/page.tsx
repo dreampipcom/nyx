3bdf1b22990f7ac53167b05574ee432b3adee421
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-26 03:13:45 +0100

    ar(feat) [DPCP-40]: Parse Services.

--- src/app/[locale]/dash/signin/page.tsx
+++ src/app/[locale]/dash/signin/page.tsx
@@ -4,13 +4,13 @@ import { providers, getCsrf } from '@auth';
 import { VSignUp } from '@components/client';
 import styles from '@styles/page.module.css';
 
-export default async function SignUp({ params }) {
+export default async function SignUp() {
   const csrf = await getCsrf();
 
   return (
     <main className={styles.main}>
       <article>
-        <VSignUp providers={providers} csrf={csrf} user={params?.user} />
+        <VSignUp providers={providers} csrf={csrf} />
       </article>
     </main>
   );
